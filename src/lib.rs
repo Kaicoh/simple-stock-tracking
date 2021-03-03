3d0efc10fb88d49ae53cec6caf7e66f23134bebc
Kanji Tanaka <ktanaka@ichain.co.jp>
2021-03-03 21:31:32 +0900

    Implement min, max functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,55 @@
 pub mod app;
 pub mod date;
+
+pub fn min(series: &[f64]) -> Option<f64> {
+    if series.is_empty() {
+        None
+    } else {
+        Some(series.iter().fold(std::f64::MAX, |m, v| v.min(m)))
+    }
+}
+
+pub fn max(series: &[f64]) -> Option<f64> {
+    if series.is_empty() {
+        None
+    } else {
+        Some(series.iter().fold(std::f64::MIN, |m, v| v.max(m)))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod fn_min {
+        use super::*;
+
+        #[test]
+        fn return_minimum_if_not_empty() {
+            let series: Vec<f64> = vec![2.1, 1.3, 3.6];
+            assert_eq!(Some(1.3), min(&series[..]));
+        }
+
+        #[test]
+        fn return_none_if_empty() {
+            let series: Vec<f64> = vec![];
+            assert_eq!(None, min(&series[..]));
+        }
+    }
+
+    mod fn_max {
+        use super::*;
+
+        #[test]
+        fn return_maximum_if_not_empty() {
+            let series: Vec<f64> = vec![2.1, 1.3, 3.6];
+            assert_eq!(Some(3.6), max(&series[..]));
+        }
+
+        #[test]
+        fn return_none_if_empty() {
+            let series: Vec<f64> = vec![];
+            assert_eq!(None, max(&series[..]));
+        }
+    }
+}
